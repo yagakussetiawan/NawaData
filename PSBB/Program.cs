8e5d69814bf8792d7980d7de858c91aa04775391
Yoga Kus Setiawan <mobilelegends090704@gmail.com>
2025-03-26 11:07:24 +0700

    test

---
+++ PSBB/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input the number of families : ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Input the number of members in the family (separated by a space) : ");
+        string[] membersInput = Console.ReadLine().Split(' ');
+        if (membersInput.Length != n)
+        {
+            Console.WriteLine("Input must be equal with count of family");
+            return;
+        }
+        int[] members = membersInput.Select(int.Parse).ToArray();
+        int busCount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (members[i] > 4)
+            {
+                busCount += (int)Math.Ceiling(members[i] / 4.0); 
+            }
+            else
+            {
+                busCount++; 
+                if (i + 1 < n && members[i] + members[i + 1] <= 4) 
+                {
+                    i++; 
+                }
+            }
+        }
+
+        Console.WriteLine($"Minimum bus required is : {busCount}");
+    }
+}
---
+++ SortCharacter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input one line of words (S) : ");
+        string input = Console.ReadLine().ToLower(); 
+        List<char> vowels = new List<char>();
+        List<char> consonants = new List<char>();
+        char[] vowelChars = { 'a', 'e', 'i', 'o', 'u' };
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c)) 
+            {
+                if (vowelChars.Contains(c))
+                    vowels.Add(c); 
+                else
+                    consonants.Add(c); 
+            }
+        }
+        Console.WriteLine("Vowel Characters : ");
+        Console.WriteLine(string.Join("", vowels));
+        Console.WriteLine("Consonant Characters : ");
+        Console.WriteLine(string.Join("", consonants));
+    }
+}
